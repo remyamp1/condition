f27ff880dca44245a94f94621e976ae591b26f3d
remyamp <remyamp774gmail.com>
2024-07-23 13:31:54 +0530

    23-7-2024

---
+++ program13.dart
@@ -0,0 +1,16 @@
+import "dart:io";
+void main(){
+stdout.write("Enter two number:");
+String? num1=stdin.readLineSync()!;
+int newnum1=int.parse(num1);
+
+String? num2=stdin.readLineSync()!;
+int newnum2=int.parse(num2);
+
+if (newnum1<newnum2){
+print("$newnum1: is small");
+}
+else {
+print("$newnum2: is small");
+}
+}
